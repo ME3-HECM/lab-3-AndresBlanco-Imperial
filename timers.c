bafbe9bb161ce6b6792f066d648ae557d7a87898
AndresBlanco-Imperial <ab3021@ic.ac.uk>
2024-02-01 19:18:13 +0000

    FInal Question 2.5

--- timers.c
+++ timers.c
@@ -8,7 +8,7 @@ void Timer0_init(void)
 {
     T0CON1bits.T0CS=0b010; // Fosc/4
     T0CON1bits.T0ASYNC=1; // see datasheet errata - needed to ensure correct operation when Fosc/4 used as clock source
-    T0CON1bits.T0CKPS=0b1000; // 1:256
+    T0CON1bits.T0CKPS=0b1000; // 1:225
     T0CON0bits.T016BIT=1;	//16bit mode	
 	
     PIE0bits.TMR0IE = 1; //enable intrerrupt
@@ -18,9 +18,9 @@ void Timer0_init(void)
     INTCONbits.PEIE = 1; // peripheral interrupt
     INTCONbits.GIE = 1; // global interrupt
             
-    // it's a good idea to initialise the timer registers so we know we are at 0
-    TMR0H=0;            //write High reg first, update happens when low reg is written to
-    TMR0L=0;
+
+    TMR0H=00001011;            //write High reg first, update happens when low reg is written to
+    TMR0L=110011011;
     T0CON0bits.T0EN=1;	//start the timer
     
     
@@ -37,13 +37,12 @@ unsigned int get16bitTMR0val(void)
     combined_value = TMR0L | (TMR0H << 8);
     return combined_value;
 }
-
 void __interrupt(low_priority) LowISR()
 {   
     if (PIR0bits.TMR0IF == 1) { // check interrupt flag
         LATHbits.LATH3 = !LATHbits.LATH3; // toggle LED
-        TMR0H = 0; // reset timer
-        TMR0L = 0;
+        TMR0H=00001011;            //write High reg first, update happens when low reg is written to
+        TMR0L=110011011;
         PIR0bits.TMR0IF = 0; // clear interrupt flag
     }
 }
