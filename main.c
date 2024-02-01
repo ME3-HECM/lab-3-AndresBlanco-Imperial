64e2b16d983d982a2fd3a28fe9f5fc76e1eb725b
AndresBlanco-Imperial <ab3021@ic.ac.uk>
2024-02-01 14:47:51 +0000

    2.1

--- main.c
+++ main.c
@@ -1,22 +1,29 @@
 // CONFIG1L
 #pragma config FEXTOSC = HS     // External Oscillator mode Selection bits (HS (crystal oscillator) above 8 MHz; PFM set to high power)
 #pragma config RSTOSC = EXTOSC_4PLL// Power-up default value for COSC bits (EXTOSC with 4x PLL, with EXTOSC operating per FEXTOSC bits)
-
 // CONFIG3L
 #pragma config WDTE = OFF        // WDT operating mode (WDT enabled regardless of sleep)
 
 #include <xc.h>
-#include "LEDarray.h"
+
+//#include "LEDarray.h"
 #include "interrupts.h"
 #include "comparator.h"
 
-#define _XTAL_FREQ 64000000 //note intrinsic _delay function is 62.5ns at 64,000,000Hz  
 
 
 void main(void) {
 	//call your initialisation functions to set up the hardware modules
 
+    TRISHbits.TRISH3 = 0;
+    LATHbits.LATH3 = 1;
+    __delay_ms(200); 
+    
+    LATHbits.LATH3 = 0;
+    DAC_init();
+    Comp1_init();
+    Interrupts_init();
     while (1) {
-		    LATDbits.LATD7 =1;
+
     }
 }
